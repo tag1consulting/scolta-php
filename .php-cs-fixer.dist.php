679a190a0ec8cde68914f6d44676a8c63750efc0
Jeremy Andrews <jeremy@tag1consulting.com>
2026-04-08 18:10:06 +0200

    Add .editorconfig, .php-cs-fixer config, composer lint/format scripts

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,18 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__ . '/src')
+    ->in(__DIR__ . '/tests');
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@PSR12' => true,
+        'strict_param' => true,
+        'declare_strict_types' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'no_unused_imports' => true,
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
+        'single_quote' => true,
+    ])
+    ->setFinder($finder)
+    ->setRiskyAllowed(true);
